bd357046294c12ab90a9e3770bbcfc595ececbb4
Sergei Atroshchenko <sergei.atroshchenko@cbsi.com>
2019-07-23 16:40:05 +0300

    bootstrap update & fix

--- src/functions.php
+++ src/functions.php
@@ -4,14 +4,3 @@ function dd($var) {
     var_dump($var);
     die();
 }
-
-function debugModeOn() {
-    global $debug;
-    
-    if ($debug !== true) {
-        error_reporting(E_ALL & ~E_NOTICE);
-        ini_set("display_errors", 1);
-        
-        $debug = true;
-    }
-}
